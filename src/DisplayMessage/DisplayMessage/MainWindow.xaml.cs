a10a6e8138dc5183cc0e679a1117d8c3d5afdd30
supershowwei <supershowwei@gmail.com>
2023-08-12 09:58:50 +0800

    complete

---
+++ src/DisplayMessage/DisplayMessage/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace DisplayMessage
+{
+    /// <summary>
+    ///     MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            var args = Environment.GetCommandLineArgs();
+
+            if (args.Length > 1)
+            {
+                this.TextBox1.Text = string.Join("\r\n\r\n", args.Skip(1));
+            }
+            else
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
