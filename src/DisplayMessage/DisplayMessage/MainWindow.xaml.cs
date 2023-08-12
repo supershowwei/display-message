c9c1d9c9144ad398f0cfc3eeec00b233f4692390
supershowwei <supershowwei@gmail.com>
2023-08-12 10:37:13 +0800

    add count down

--- src/DisplayMessage/DisplayMessage/MainWindow.xaml.cs
+++ src/DisplayMessage/DisplayMessage/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace DisplayMessage
@@ -20,7 +21,29 @@ namespace DisplayMessage
 
             if (args.Length > 1)
             {
-                this.TextBox1.Text = string.Join("\r\n\r\n", args.Skip(1));
+                string message;
+
+                if (int.TryParse(args[1], out var seconds))
+                {
+                    message = string.Join("\r\n\r\n", args.Skip(2));
+
+                    Task.Delay(TimeSpan.FromSeconds(seconds))
+                        .ContinueWith(
+                            _ =>
+                                {
+                                    this.Dispatcher.Invoke(
+                                        () =>
+                                            {
+                                                Application.Current.Shutdown();
+                                            });
+                                });
+                }
+                else
+                {
+                    message = string.Join("\r\n\r\n", args.Skip(1));
+                }
+
+                this.TextBox1.Text = message;
             }
             else
             {
