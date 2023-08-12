27295854533420faec0a1988766355b66e8676bf
supershowwei <supershowwei@gmail.com>
2023-08-12 10:38:16 +0800

    refactoring

--- src/DisplayMessage/DisplayMessage/MainWindow.xaml.cs
+++ src/DisplayMessage/DisplayMessage/MainWindow.xaml.cs
@@ -21,11 +21,9 @@ namespace DisplayMessage
 
             if (args.Length > 1)
             {
-                string message;
-
                 if (int.TryParse(args[1], out var seconds))
                 {
-                    message = string.Join("\r\n\r\n", args.Skip(2));
+                    this.TextBox1.Text = string.Join("\r\n\r\n", args.Skip(2));
 
                     Task.Delay(TimeSpan.FromSeconds(seconds))
                         .ContinueWith(
@@ -40,10 +38,8 @@ namespace DisplayMessage
                 }
                 else
                 {
-                    message = string.Join("\r\n\r\n", args.Skip(1));
+                    this.TextBox1.Text = string.Join("\r\n\r\n", args.Skip(1));
                 }
-
-                this.TextBox1.Text = message;
             }
             else
             {
